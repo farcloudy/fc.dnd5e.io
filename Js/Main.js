b671815fc81a6f50b9749de4d085583e496cce29
farcloudy <1808274337@qq.com>
2022-05-03 13:55:06 +0800

    DataBase

--- Js/Main.js
+++ Js/Main.js
@@ -1,3 +1,11 @@
-function JUMP(page){
-	var c1 = 
+function SHElement(Eid, Ety){
+	var TButton = 'span' + Eid;
+	var Target = Ety + Eid;
+	if ($(TButton).text() == '展开'){
+		$(Ety).show();
+		$(TButton).text("折叠");
+	} else{
+		$(Ety).hide();
+		$(TButton).text("展开");
+	}
 }
