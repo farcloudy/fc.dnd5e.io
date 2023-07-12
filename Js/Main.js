df9b25b25f5796a2abd75e57135f571e320eac01
farcloudy <38643907+farcloudy@users.noreply.github.com>
2023-07-12 16:51:02 +0800

    0712
    1

--- Js/Main.js
+++ Js/Main.js
@@ -1,11 +1,37 @@
-function SHElement(Eid, Ety){
-	var TButton = 'button' + Eid;
-	var Target = Ety + Eid;
-	if ($(TButton).text() == '展开'){
-		$(Ety).show();
-		$(TButton).text("折叠");
-	} else{
-		$(Ety).hide();
-		$(TButton).text("展开");
+function SetWindowCenter(element){
+	//console.log(1);
+	var parentHeight = $(element).parent().innerHeight();
+	var parentWidth = $(element).parent().innerWidth();
+	//console.log(parentHeight);
+
+	var selfHeight = $(element).css('height').slice(0, -2);
+	var selfWidth = $(element).css('width').slice(0, -2);
+	//console.log(selfHeight);
+
+	$(element).css('top', (parentHeight - selfHeight) / 2);
+	$(element).css('left', (parentWidth - selfWidth) / 2);
+}
+
+function onLoad(){
+	SetWindowCenter('div.Main');
+}
+
+function testshowlist(actorId){
+	$('div#cardList').html(
+		getCardByIndexList(actorList[actorId]['cardList'])
+	);
+
+}
+
+function startMenu_Staff(){
+	$('div.Main').children().css('display', 'none');
+	$('div.Main').html("")
+}
+
+function getCardByIndexList(indexList){
+	var text = ""
+	for (id in indexList){
+		text = text + "<div>" + cardList[id].name + "</div>"
 	}
+	return text
 }
---
+++ Js/data_Card.js
@@ -0,0 +1,9 @@
+var cardList = [
+	{"id":0, "name":"攻击", "atk":1},
+    {"id":1, "name":"格挡", "atk":1}
+];
+
+var actorList = [
+    {'id':0, 'name':"杨续", 'cardList':[0, 0]}
+]
+
